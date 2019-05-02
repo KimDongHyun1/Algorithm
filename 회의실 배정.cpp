9e28ec20748161cfb89f948d3f7be02e43d6622c
donghyun <donghyun4012@naver.com>
2019-05-02 16:22:32 +0900

    그리디

---
+++ 회의실 배정.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+struct Meeting {
+	int begin, end;
+};
+
+bool cmp(const Meeting &u, const Meeting &v) {
+	if (u.end == v.end) {
+		return u.begin < v.begin;
+	}
+	else {
+		return u.end < v.end;
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector<Meeting> a(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i].begin >> a[i].end;
+	}
+
+	sort(a.begin(), a.end(), cmp);
+
+	int now = 0;
+	int ans = 0;
+	for (int i = 0; i < a.size(); i++) {
+		if (now <= a[i].begin) {
+			now = a[i].end;
+			ans += 1;
+		}
+	}
+	cout << ans << '\n';
+}
