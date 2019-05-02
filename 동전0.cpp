6868d4ed29ad695e8a5c38025c391fe02966f6fd
donghyun <donghyun4012@naver.com>
2019-05-02 16:16:52 +0900

    1

---
+++ 동전0.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+	int n, k;
+	int arr[11];
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++) {
+		int x;
+		cin >> x;
+		arr[i] = x;
+	}
+
+	int cnt = 0;
+
+	for (int i = n; i >= 1; i--) {
+		if (k >= arr[i]) {
+			cnt += k / arr[i];
+			k %= arr[i];
+		}
+	}
+
+	cout << cnt << '\n';
+}
