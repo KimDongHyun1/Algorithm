e44210996b5a0b2a0727e67d9aebf21e2f8eb8d5
donghyun <donghyun4012@naver.com>
2019-05-02 16:27:26 +0900

    그리디

---
+++ ATM.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end());
+
+	int now = 0;
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		now = now + a[i];
+		ans = ans + now;
+	}
+	cout << ans << '\n';
+}
