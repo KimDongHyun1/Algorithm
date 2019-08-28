70e7b737d802e38a58783adf59205d4cb3371f6c
donghyun <donghyun4012@naver.com>
2019-08-28 18:29:28 +0900

    DP

---
+++ 암호코드.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+int dp[501];
+
+int main() {
+	string s;
+	cin >> s;
+	s = " " + s;
+
+	dp[0] = 1;
+	for (int i = 1; i < s.size(); i++) {
+		int x = s[i] - '0';
+
+		if (1<=x && x<=9) {
+			dp[i] = (dp[i] + dp[i - 1]) % 1000000;
+		}
+
+		if (i == 1) continue;
+		if (s[i - 1] == '0') continue;
+
+		x = (s[i - 1] - '0') * 10 + (s[i] - '0');
+		if (10 <= x && x <= 26) {
+			dp[i] = (dp[i] + dp[i - 2]) % 1000000;
+		}
+
+	}
+
+	cout << dp[s.size()-1] << '\n';
+}
