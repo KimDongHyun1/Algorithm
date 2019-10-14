5c54592ecc5904eb805a3adff824228655dc2191
donghyun <donghyun4012@naver.com>
2019-10-14 20:48:25 +0900

    순열

---
+++ 스타트와 링크.cpp
@@ -0,0 +1,54 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<vector<int>> arr(n, vector<int>(n));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	vector<int> perm(n);
+	for (int i = 0; i < n / 2; i++) {
+		perm[i] = 1;
+	}
+
+	sort(perm.begin(), perm.end());
+	int ans = 2147483647;
+
+	do {
+		vector<int> first, second;
+		for (int i = 0; i < n; i++) {
+			if (perm[i] == 0) {
+				first.push_back(i);
+			}
+			else {
+				second.push_back(i);
+			}
+		}
+
+		int one = 0;
+		int two = 0;
+		for (int i = 0; i < n / 2; i++) {
+			for (int j = 0; j < n / 2; j++) {
+				if (i == j) continue;
+				one += arr[first[i]][first[j]];
+				two += arr[second[i]][second[j]];
+			}
+		}
+
+		int diff = one - two;
+		if (diff < 0) diff = -diff;
+		if (ans > diff) ans = diff;
+	} while (next_permutation(perm.begin(), perm.end()));
+
+	cout << ans << '\n';
+	return 0;
+}
---
+++ 연산자 끼워넣기.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> num(n);
+	vector<int> sign;
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	for (int i = 0; i < 4; i++) {
+		int cnt = 0;
+		cin >> cnt;
+		for (int j = 0; j < cnt; j++) {
+			sign.push_back(i);
+		}
+	}
+
+	int sum = num[0];
+	for (int i = 0; i < sign.size(); i++) {
+		if (sign[i] == 0) sum = sum + num[i + 1];
+		if (sign[i] == 1) sum = sum - num[i + 1];
+		if (sign[i] == 2) sum = sum * num[i + 1];
+		if (sign[i] == 3) sum = sum / num[i + 1];
+	}
+
+	int min = sum;
+	int max = sum;
+
+	do {
+		int sum = num[0];
+
+		for (int i = 0; i < sign.size(); i++) {
+			if (sign[i] == 0) sum = sum + num[i + 1];
+			if (sign[i] == 1) sum = sum - num[i + 1];
+			if (sign[i] == 2) sum = sum * num[i + 1];
+			if (sign[i] == 3) sum = sum / num[i + 1];
+		}
+
+		if (max < sum) max = sum;
+		if (min > sum) min = sum;
+
+	} while (next_permutation(sign.begin(), sign.end()));
+
+	cout << max << '\n' << min << '\n';
+}
