88be339dfb0164ee4ab02264b41bd01ea38a70af
donghyun <donghyun4012@naver.com>
2019-10-16 01:48:36 +0900

    재귀

---
+++ 로또.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> lotto;
+
+void check(vector<int> &a, int index, int cnt) {
+	if (cnt == 6) {
+		for (int i = 0; i < lotto.size(); i++) {
+			cout << lotto[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	if (a.size() == index) return;
+	lotto.push_back(a[index]);
+	check(a, index + 1, cnt + 1);
+	lotto.pop_back();
+	check(a, index + 1, cnt);
+}
+
+int main() {
+
+	while (true) {
+		int k;
+		cin >> k;
+		if (k == 0) break;
+		vector<int> a(k);
+		for (int i = 0; i < k; i++) {
+			cin >> a[i];
+		}
+
+		check(a, 0, 0);
+		cout << '\n';
+	}
+}
---
+++ 부분수열의 합.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int cnt = 0;
+
+void rec(vector<int> &a, int index, int sum, int s) {
+	if (sum == s) {
+		cnt++;
+	}
+
+	if (index >= a.size()-1 ) return;
+	rec(a, index + 1, sum - a[index] + a[index + 1], s);
+	rec(a, index + 1, sum + a[index + 1], s);
+}
+
+int main() {
+	int n, s;
+	cin >> n >> s;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	rec(a, 0, a[0], s);
+
+	cout << cnt << '\n';
+}
