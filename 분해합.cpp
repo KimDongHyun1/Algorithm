42c86309d4e3f46d8432231906587b2c02884cda
donghyun <donghyun4012@naver.com>
2019-10-29 23:56:33 +0900

    브루트 포스

---
+++ 분해합.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	// cout << 189 % 10 << '\n'; // 9
+	// cout << 189 / 10 << '\n'; // 18
+
+	int n;
+	cin >> n;
+	int ans = 0;
+
+	for (int i = 1; i <= n; i++) {
+		int sum = i;
+		int mok = i;
+		do {
+			sum = sum + mok % 10;
+			mok = mok / 10;
+		} while (mok);
+		if (sum == n) {
+			ans = i;
+			break;
+		}
+	}
+
+	cout << ans << '\n';
+
+}
+
---
+++ 블랙잭.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end());
+	int ans = m;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			for (int k = j + 1; k < n; k++) {
+				int sum = a[i] + a[j] + a[k];
+				if ((m - sum >= 0) && (m - sum < ans)) ans = m - sum;
+			}
+		}
+	}
+
+	cout << m - ans << '\n';
+}
