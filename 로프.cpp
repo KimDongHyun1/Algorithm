f1ca6ca0de089dd996342afba8ef9a9690e86da5
donghyun <donghyun4012@naver.com>
2019-11-04 21:20:21 +0900

    그리디

---
+++ 로프.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int a[100001];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a, a + n);
+	long long ans = 0;
+	for (int i = 0; i < n; i++) {
+		long long sum = a[i];
+		for (int j = i + 1; j < n; j++) {
+			sum = sum + a[i];
+		}
+		if (ans < sum) ans = sum;
+	}
+
+	cout << ans << '\n';
+	return 0;
+}
