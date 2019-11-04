53f20c50c5d5904b783e17d29f90b3a5926f074f
donghyun <donghyun4012@naver.com>
2019-11-04 15:58:10 +0900

    DP

--- LCS.cpp
+++ LCS.cpp
@@ -11,10 +11,10 @@ int main() {
 	b[0] = 'b';
 	cin >> a + 1;
 	cin >> b + 1;
-
+	
 	dp[0][0] = 0;
-	for (int i = 1; i < strlen(a); i++) {
-		for (int j = 1; j < strlen(b); j++) {
+	for (int i = 1; i<strlen(a); i++) {
+		for (int j = 1; j<strlen(b); j++) {
 			if (a[i] == b[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
 			else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 		}
---
+++ 평범한 배낭.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int dp[101][100001];
+int w[101];
+int v[101];
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		cin >> w[i] >> v[i];
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			dp[i][j] = dp[i - 1][j]; // 지금까지 있는 것 중에 MAX 값
+			if (j - w[i] >= 0) { // 만약에 현재 N값의 무게가 KEY 값보다 작다면
+				dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]); // 지금까지의 MAX값 vs W를 뺸 전꺼의 MAX 값 + 현재 N의 VAL의값 
+			}
+		}
+	}
+
+	cout << dp[n][k] << '\n';
+	return 0;
+}
