3119504c350a8623f54d85638583715abbe3e98f
donghyun <donghyun4012@naver.com>
2019-11-04 16:24:48 +0900

    DP

--- 평범한 배낭.cpp
+++ 평범한 배낭.cpp
@@ -1,5 +1,4 @@
-﻿#include "pch.h"
-#include <iostream>
+﻿#include <iostream>
 #include <algorithm>
 #include <cstring>
 using namespace std;
@@ -18,8 +17,8 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= k; j++) {
 			dp[i][j] = dp[i - 1][j]; // 지금까지 있는 것 중에 MAX 값
-			if (j - w[i] >= 0) { // 만약에 현재 N값의 무게가 KEY 값보다 작다면
-				dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]); // 지금까지의 MAX값 vs W를 뺸 전꺼의 MAX 값 + 현재 N의 VAL의값 
+			if (j - w[i] >= 0) {
+				dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
 			}
 		}
 	}
