c2588fc09ff71368db27b5a9041017b2373e3a48
donghyun <donghyun4012@naver.com>
2019-11-10 01:53:41 +0900

    큐

---
+++ 가운데를 말해요.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	priority_queue<int> big;
+	priority_queue<int, vector<int>, greater<int>> small;
+
+	for (int i = 0; i < n; i++) {
+		int cur;
+		cin >> cur;
+
+		if (big.size() > small.size()) small.push(cur);
+		else big.push(cur);
+
+		if (!small.empty() && big.top() < small.top()) {
+			int smallTop = small.top();
+			int bigTop = big.top();
+			small.pop(); big.pop();
+			big.push(smallTop);
+			small.push(bigTop);
+		}
+
+
+		cout << big.top() << '\n';
+	}
+}
