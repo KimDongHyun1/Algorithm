898294860c0cc546e4100cea873896dc4795d969
donghyun <donghyun4012@naver.com>
2019-11-09 18:35:31 +0900

    이분 탐색

---
+++ K번째 수.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n;
+	cin >> k;
+
+	int left = 1;
+	int right = k;
+	int ans = 1;
+
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		int num = 0;
+		for (int i = 1; i <= n; i++) {
+			num += min(mid / i, n);
+		}
+
+		if (num < k) {
+			left = mid + 1;
+		}
+		else {
+			ans = mid;
+			right = mid - 1;
+		}
+
+	}
+
+	cout << ans << '\n';
+	return 0;
+}
---
+++ 가장 긴 증가하는 부분 수열 2.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> v;
+
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		int cur;
+		cin >> cur;
+
+		if (i == 0 || cur > v.back()) {
+			v.push_back(cur);
+			cnt++;
+		}
+		else {
+			auto iter = lower_bound(v.begin(), v.end(), cur);
+			*iter = cur;
+		}
+	}
+
+	cout << cnt << '\n';
+	return 0;
+}
