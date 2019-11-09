257e81a8ad38502ca844f3f5db50c6aeacba58ea
donghyun <donghyun4012@naver.com>
2019-11-10 03:28:25 +0900

    DP

---
+++ 동전 1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int a[101];
+int dp[10001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, k;
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) cin >> a[i];
+	sort(a, a + n + 1);
+
+	dp[0] = 1;
+	for (int i = 1; i <= n; i++) {
+		for (int j = a[i]; j <= k; j++) {
+			dp[j] = dp[j] + dp[j - a[i]];
+		}
+	}
+
+	cout << dp[k] << '\n';
+	return 0;
+}
