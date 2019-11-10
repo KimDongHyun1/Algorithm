aa383cdb30667c3cbab971d179ce339f75f8c134
donghyun <donghyun4012@naver.com>
2019-11-10 22:13:49 +0900

    DP

---
+++ 행렬 곱셈 순서.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[501][501];
+int n, r, c;
+
+typedef struct {
+	int r;
+	int c;
+} st;
+
+st cost[501];
+
+int main()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int x, y;
+		cin >> x >> y;
+		cost[i].r = x;
+		cost[i].c = y;
+	}
+
+	for (int d = 1; d < n; d++) {
+		for (int a = 1; a <= n - d; a++) {
+			int b = a + d;
+			dp[a][b] = INT_MAX;
+			
+			for (int mid = a; mid < b; mid++) {
+				dp[a][b] = min(dp[a][b], dp[a][mid] + dp[mid + 1][b] + cost[a].r * cost[mid].c * cost[b].c);
+			}
+		}
+	}
+
+	cout << dp[1][n] << '\n';
+}
