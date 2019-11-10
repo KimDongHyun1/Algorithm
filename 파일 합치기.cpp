6dfb213a5c90eb17b71e5cab793668c4e39b12ab
donghyun <donghyun4012@naver.com>
2019-11-10 22:12:14 +0900

    DP

--- 파일 합치기.cpp
+++ 파일 합치기.cpp
@@ -5,32 +5,30 @@ using namespace std;
 int INF = 2000000000;
 int cost[501];
 int dp[501][501];
-int sum[501];
 
 int main() {
-	int t;
-	cin >> t;
+	int n;
+	cin >> n;
+	vector<pair<int, int>> v(n);
+	for (int i = 0; i < n; i++) {
+		int row, col;
+		cin >> row >> col;
+		v.push_back(make_pair(row, col));
+	}
 
-	while (t--) {
-		int k;
-		cin >> k;
-		for (int i = 1; i <= k; i++) {
-			cin >> cost[i];
-			sum[i] = sum[i - 1] + cost[i];
-		}
-
-		for (int d = 1; d < k; d++) {
-			for (int a = 1; a + d <= k; a++) {
-				int b = a + d;
-				dp[a][b] = INF;
-				for (int mid = a; mid < b; mid++) {
-					dp[a][b] = min(dp[a][b], dp[a][mid] + dp[mid + 1][b] + sum[b] - sum[a - 1]);
-				}
+	for (int d = 1; d < n; d++) {
+		for (int a = 0; a + d <= n; a++) {
+			int b = a + d;
+			dp[a][b] = INF;
+			
+			for (int mid = a; mid < b; mid++) {
+				dp[a][b] =
+					min(dp[a][b], dp[a][mid] + dp[mid + 1][b] + cost[a].first*cost[a].second*cost[b].second);
 			}
 		}
-
-		cout << dp[1][k] << '\n';
 	}
+
+	cout << dp[0][n - 1] << '\n';
+
 	
-	return 0;
 }
