b91520bd8210ebf6394ef22d0b5c7e9ca40c871f
donghyun <donghyun4012@naver.com>
2019-11-15 21:40:59 +0900

    BFS

---
+++ 숨바꼭질.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int n, k;
+int check[100001];
+
+int main()
+{
+	cin >> n >> k;
+	queue<int> q;
+	q.push(n);
+
+	while (!q.empty() && q.front() != k) {
+		int x = q.front();
+		q.pop();
+		int num1 = x - 1;
+		int num2 = x + 1;
+		int num3 = x * 2;
+		if (num1 >= 0 && !check[num1]) {
+			check[num1] = check[x] + 1;
+			q.push(num1);
+		}
+		if (num2 <= 100000 && !check[num2]) {
+			check[num2] = check[x] + 1;
+			q.push(num2);
+		}
+		if (num3 <= 100000 && !check[num3]) {
+			check[num3] = check[x] + 1;
+			q.push(num3);
+		}
+
+	}
+
+	cout << check[k] << '\n';
+}
