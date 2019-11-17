852d4d826e230fcd147f599ad6b9867d1f889096
donghyun <donghyun4012@naver.com>
2019-11-17 17:12:13 +0900

    브루트포스

---
+++ 일곱 난쟁이.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int cost[10];
+
+int main()
+{
+	for (int i = 0; i < 9; i++) cin >> cost[i];
+	sort(cost, cost+9);
+
+	do {
+		int sum = 0;
+		for (int i = 0; i < 7; i++) {
+			sum += cost[i];
+		}
+		if (sum == 100) break;
+		
+	} while (next_permutation(cost, cost + 9));
+
+	for (int i = 0; i < 7; i++) {
+		cout << cost[i] << ' ';
+	}
+
+	cout << '\n';
+	return 0;
+}
+
