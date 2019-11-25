600b929db068e83645192f973efaf84d1f605ecb
donghyun <donghyun4012@naver.com>
2019-11-25 14:56:00 +0900

    시뮬레이션

---
+++ 프린터 큐.cpp
@@ -0,0 +1,52 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <queue>
+using namespace std;
+int t, n, m, x , cnt;
+
+int main() {
+	cin >> t;
+
+	while (t--) {
+		cin >> n >> m;
+		queue<pair<int, int>> Q;
+		priority_queue<int> PQ;
+		for (int i = 0; i < n; i++) {
+			cin >> x;
+			Q.push(make_pair(i, x));
+			PQ.push(x);
+		}
+
+		cnt = 1;
+		while (true) {
+			int index = Q.front().first;
+			int value = Q.front().second;
+
+			if (m == index) {
+				if (PQ.top() == value) {
+					break;
+				}
+
+				Q.pop();
+				Q.push(make_pair(index, value));
+			}
+			else {
+				if (PQ.top() == value) {
+					Q.pop();
+					PQ.pop();
+					cnt++;
+				}
+				else {
+					Q.pop();
+					Q.push(make_pair(index, value));
+				}
+			}
+
+		}
+
+		cout << cnt << '\n';
+	}
+
+	return 0;
+	
+}
