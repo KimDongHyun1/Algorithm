69ed0ab5d9d48d8d31bf29e88aaaea66100e29b6
donghyun <donghyun4012@naver.com>
2019-12-03 22:02:22 +0900

    퇴사

--- 퇴사.cpp
+++ 퇴사.cpp
@@ -1,34 +1,38 @@
-﻿#include <iostream>
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
 using namespace std;
-int n;
-int time[22];
-int price[22];
-int ans = 0;
+int dp[1001];
+int t[1001];
+int p[1001];
 
-void check(int day, int sum) {
-	if (day == n + 1) {
-		if (sum > ans) ans = sum;
-		return;
-	}
-	if (day > n + 1) {
-		return;
-	}
-
-	check(day + 1, sum);
-	check(day + time[day], sum + price[day]);
-}
-
-int main() {
+int main()
+{
+	int n;
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
-		cin >> time[i];
-		cin >> price[i];
+		cin >> t[i] >> p[i];
 	}
 
-	check(1, 0);
-	cout << ans << '\n';
+	// 만약 1이라면 dp[i-1] + a[i] , j탐색-> a[j] + dp[j]
+	// 아니라면 dp[i-1] , j탐색- a[j] + dp[j]
+	for (int i = 1; i <= n; i++) {
+		if (t[i] == 1) {
+			dp[i] = dp[i - 1] + p[i];
+		}
+		else {
+			dp[i] = dp[i - 1];
+		}
+
+		for (int j = 1; j < i; j++) {
+			if (t[j] + j - 1 == i) {
+				dp[i] = max(dp[i], dp[j-1] + p[j]);
+			}
+		}
+
+	}
+	
+	cout << dp[n] << '\n';
 	return 0;
 }
-
-
 
