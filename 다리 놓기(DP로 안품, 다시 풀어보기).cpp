09fa4b4827e45163f502e6fa08127eac9e0f8a48
donghyun <donghyun4012@naver.com>
2019-12-04 22:01:29 +0900

    DP

---
+++ 다리 놓기(DP로 안품, 다시 풀어보기).cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n, m;
+		cin >> n >> m;
+
+		
+		long long share = 1;
+		for (int i = m; i > m - n; i--) {
+			share *= i;
+		}
+		for (int i = 2; i <= n; i++) {
+			share /= i;
+		}
+			
+		cout << share << '\n';
+	}
+
+
+	return 0;
+}
+
