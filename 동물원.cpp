c723d24e3340a1e1908fcbf8469bde80ae116821
donghyun <donghyun4012@naver.com>
2019-12-06 17:33:52 +0900

    DP

--- 동물원.cpp
+++ 동물원.cpp
@@ -1,10 +1,24 @@
 ﻿#include "pch.h"
 #include <iostream>
 using namespace std;
-int dp[100001];
+int dp[100001][3]; // 0은 이전줄이"00" , 1은 이전줄이"10", 2는 이전줄이"01"
 
 int main()
 {
+	int n;
+	cin >> n;
+	
+	dp[0][0] = 1;
+	for (int i = 1; i <= n; i++) {
+		dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+		dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+		dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+	}
+
+	cout << (dp[n][0] + dp[n][1] + dp[n][2]) % 9901 << '\n';
+	return 0;
+
+	/*
 	int n;
 	cin >> n;
 
@@ -16,6 +30,5 @@ int main()
 
 	cout << dp[n] << '\n';
 	return 0;
+	*/
 }
-
-
