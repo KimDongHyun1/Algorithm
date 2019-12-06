7f0acfb8555bec4637e5a9b3f1d41f63b01ced68
donghyun <donghyun4012@naver.com>
2019-12-06 23:13:15 +0900

    DP

---
+++ 상자넣기.cpp
@@ -0,0 +1,30 @@
+﻿// 증가하는 수열인가 그거랑 아예 똑같음
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[1001];
+int a[1001];
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	for (int i = 0; i < n; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (a[j] < a[i])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans << '\n';
+}
---
+++ 욕심쟁이 판다.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int dx[] = { 0,0,1,-1 };
+int dy[] = { 1,-1,0,0 };
+int a[501][501];
+int visited[501][501];
+int n, ans;
+	
+int dfs(int x, int y) {
+	if (visited[x][y]) return visited[x][y];
+
+	visited[x][y] = 1;
+	for (int k = 0; k < 4; k++) {
+		int nx = x + dx[k];
+		int ny = y + dy[k];
+
+		if (nx >= 0 && nx < n && ny >= 0 && ny < n && a[x][y] < a[nx][ny]) {
+			visited[x][y] = max(visited[x][y], dfs(nx, ny) + 1);
+		}
+	}
+
+
+	return visited[x][y];
+}
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!visited[i][j]) {
+				ans = max(ans, dfs(i, j));
+			}
+		}
+	}
+
+	cout << ans << '\n';
+	return 0;
+}
+
