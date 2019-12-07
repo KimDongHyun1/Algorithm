7401b4053ef6bbba26f4d02d78f8dc4b2c083d2b
donghyun <donghyun4012@naver.com>
2019-12-07 19:23:09 +0900

    DP

---
+++ 가장 큰 정사각형.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+int a[1001][1001];
+int dp[1001][1001];
+int n, m, ans;
+
+int main()
+{
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		string s;
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) {
+			a[i][j + 1] = s[j] - '0';
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (a[i][j]) {
+				dp[i][j] = min({ dp[i - 1][j - 1],dp[i - 1][j],dp[i][j - 1] }) + 1;
+			}
+
+			ans = max(ans, dp[i][j]);
+		}
+	}
+
+	cout << ans * ans << '\n';
+
+	
+}
+
