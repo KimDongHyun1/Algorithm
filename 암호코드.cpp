230a832947146e86d074ae2b7edc870d16be2678
donghyun <donghyun4012@naver.com>
2019-12-09 15:09:13 +0900

    DP

--- 암호코드.cpp
+++ 암호코드.cpp
@@ -1,32 +1,34 @@
-﻿#include "pch.h"
-#include <iostream>
-#include <cstdio>
+﻿#include <iostream>
+#include <string>
+#define MAX_LEN 1000000
 using namespace std;
-int gcd(int a, int b) {
-	while (b != 0) {
-		int temp = a % b;
-		a = b;
-		b = temp;
-	}
-	return a;
-}
+int dp[5001];
 
-int main() {
-	int ans = 0;
+int main()
+{
+	string s;
+	cin >> s; 
+	s = "0" + s; // 0 2 5 1 1 4
 
-	int n;
-	cin >> n;
-	int arr[101];
+	dp[0] = 1;
+	for (int i = 1; i < s.size(); i++) {
+		int pre = s[i - 1] - '0';
+		int now = s[i] - '0';
 
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
+		if ((i == 1 && now == 0) || (now < 0 || now>9)) { cout << 0 << '\n'; return 0; }
+		
 
-	for (int i = 0; i < n; i++) {
-		for (int j = i; j < n; j++) {
-			ans += gcd(arr[i], arr[j]);
+		if (now == 0) {
+			dp[i] = dp[i - 2];
+		}
+		else if ((pre==1) || (pre==2 && now<=6)) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % MAX_LEN;
+		}
+		else {
+			dp[i] = dp[i - 1];
 		}
 	}
 
-	cout << ans << '\n';
+	cout << dp[s.size() - 1] << '\n';
 }
+
