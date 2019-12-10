267dfc4cf88781a74efb15861daadb07870f8ea1
donghyun <donghyun4012@naver.com>
2019-12-10 16:44:02 +0900

    DP

---
+++ 격자상의 경로.cpp
@@ -0,0 +1,71 @@
+﻿// 이거 막 X*X 이거 조합으로 바로 구할 수 있는거 있었는데 찾아보기
+
+#include "pch.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+int dx[] = { 0,1 };
+int dy[] = { 1,0 };
+int dp[15][15];
+int n, m, k, a, b, ans1, ans2;
+
+int dfs(int x, int y) {
+	if (x == a && y == b) return 1;
+	if (dp[x][y] != -1) return dp[x][y];
+
+	dp[x][y] = 0;
+	for (int k = 0; k < 2; k++) {
+		int nx = x + dx[k];
+		int ny = y + dy[k];
+
+		if (nx >= 0 && nx < n && ny >= 0 && ny < m && nx <= a && ny <= b) {
+			dp[x][y] += dfs(nx, ny);
+		}
+
+	}
+
+	return dp[x][y];
+}
+
+int dfs2(int x, int y) {
+	if (x == n - 1 && y == m - 1) return 1;
+	if (dp[x][y] != -1) return dp[x][y];
+
+	dp[x][y] = 0;
+	for (int k = 0; k < 2; k++) {
+		int nx = x + dx[k];
+		int ny = y + dy[k];
+
+		if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+			dp[x][y] += dfs2(nx, ny);
+		}
+	}
+
+	return dp[x][y];
+}
+
+int main()
+{
+	cin >> n >> m >> k;
+	
+	// k의 좌표 구하기
+	// 이거 말고도 2중 for문 돌려서 Count++로 해줄 수 있음
+	a = (k - 1) / m;
+	b = (k - 1) % m;
+
+	if (k == 0) {
+		memset(dp, -1, sizeof(dp));
+		cout << dfs2(0, 0) << '\n';
+	}
+	else {
+		memset(dp, -1, sizeof(dp));
+		ans1 = dfs(0, 0);
+
+		memset(dp, -1, sizeof(dp));
+		ans2 = dfs2(a, b);
+
+		cout << ans1 * ans2 << '\n';
+	}
+	
+	return 0;
+}
---
+++ 팰린드롬.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <cstring>
+#include <iostream>
+using namespace std;
+long long a[2001];
+long long dp[2001][2001];
+int n, m, x, y;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int y = 1; y <= n; y++) {
+		cin >> a[y];
+
+		for (int x = 1; x <= y; x++) {
+			if (x == y) { dp[x][y] = 1; continue; }
+
+			// D가 1이면 그냥 두개만 비교 해주면 됨
+			if (x + 1 == y) {
+				if (a[x] == a[y]) dp[x][y] = 1;
+				else dp[x][y] = 0;
+			}
+			// 자기랑 비교할꺼 빼고 나머지가 1이면 자기랑비교만 해주면 됨
+			else {
+				if (dp[x + 1][y - 1] == 1 && a[x] == a[y]) dp[x][y] = 1;
+				else dp[x][y] = 0;
+			}
+			
+		}
+
+	}
+
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> x >> y;
+		cout << dp[x][y] << '\n';
+	}
+}
