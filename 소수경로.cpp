c012b905c84916c67eb97a36176db2bbef29c206
donghyun <donghyun4012@naver.com>
2019-12-27 01:06:47 +0900

    DP

---
+++ 소수경로.cpp
@@ -0,0 +1,95 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+using namespace std;
+int visit[10000];
+string a, b;
+int cnt, temp;
+
+bool prime(int n) {
+	for (int i = 2; i < n; i++) {
+		if (n%i == 0) return false;
+	}
+	return true;
+}
+
+void solve() {
+
+	visit[atoi(b.c_str())] = 1;
+	queue<int> Q;
+	Q.push(atoi(b.c_str()));
+
+	cnt = 0;
+	while (!Q.empty()) {
+		int size = Q.size();
+
+		while (size--) {
+			int x = Q.front();
+			string num = to_string(x);
+			Q.pop();
+
+			if (num == a) {
+				cout << cnt << '\n';
+				return;
+			}
+
+			for (int i = 1; i <= 9; i++) {
+				temp = i * 1000 + (num[1] - '0') * 100 + (num[2] - '0') * 10 + (num[3] - '0');
+
+				if (!visit[temp] && prime(temp)) {
+					visit[temp] = true;
+					Q.push(temp);
+				}
+			}
+			for (int i = 0; i <= 9; i++) {
+				temp = (num[0] - '0') * 1000 + i * 100 + (num[2] - '0') * 10 + (num[3] - '0');
+
+				if (!visit[temp] && prime(temp)) {
+					visit[temp] = true;
+					Q.push(temp);
+				}
+			}
+			for (int i = 0; i <= 9; i++) {
+				temp = (num[0] - '0') * 1000 + (num[1] - '0') * 100 + i * 10 + (num[3] - '0');
+
+				if (!visit[temp] && prime(temp)) {
+					visit[temp] = true;
+					Q.push(temp);
+				}
+			}
+			for (int i = 0; i <= 9; i++) {
+				temp = (num[0] - '0') * 1000 + (num[1] - '0') * 100 + (num[2] - '0') * 10 + i;
+
+				if (!visit[temp] && prime(temp)) {
+					visit[temp] = true;
+					Q.push(temp);
+				}
+			}
+		}
+		cnt++;
+	}
+	
+	cout << "impossible" << '\n';
+}
+
+
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--) {
+		memset(visit, 0, sizeof(visit));
+
+		cin >> a;
+		cin >> b;
+
+		solve();
+	}
+
+
+}
+
---
+++ 촌수계산.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+int visit[101];
+bool a[101][101];
+int N, X, Y, M;
+int ans = -1;
+queue<int> Q;
+
+int main()
+{
+	cin >> N;
+	cin >> X >> Y;
+	cin >> M;
+
+	for (int i = 0; i < M; i++) {
+		int x, y;
+		cin >> x >> y;
+		a[x][y] = a[y][x] = true;
+	}
+
+	Q.push(X);
+	while (!Q.empty()) {
+		int x = Q.front();
+		Q.pop();
+		if (x == Y) {
+			ans = visit[x];
+			break;
+		}
+
+		for (int y = 1; y <= N; y++) {
+			if (!visit[y] && a[x][y]) {
+				visit[y] = visit[x] + 1;
+				Q.push(y);
+			}
+		}
+	}
+
+	cout << ans << '\n';
+	return 0;
+}
