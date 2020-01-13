710769125198a6dfd9d31db40e0d8467bed6851a
donghyun <donghyun4012@naver.com>
2020-01-13 16:50:03 +0900

    BFS

---
+++ 상범 빌딩.cpp
@@ -0,0 +1,80 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cstring>
+using namespace std;
+int dz[] = { 0,0,0,0,1,-1 };
+int dx[] = { 0,0,1,-1,0,0 };
+int dy[] = { 1,-1,0,0,0,0 };
+int visit[31][31][31];
+char map[31][31][31];
+int L, R, C, sz, sx, sy, cnt;
+
+int main()
+{
+	while (true) {
+		memset(visit, 0, sizeof(visit));
+		memset(map, 0, sizeof(map));
+		bool Flag = false;
+		cnt = 0;
+		cin >> L >> R >> C;
+		
+		if (L == R  && R == C  && C == 0) return 0;
+
+		for (int i = 0; i < L; i++) {
+			for (int j = 0; j < R; j++) {
+				string s;
+				cin >> s;
+				for (int k = 0; k < s.size(); k++) {
+					map[i][j][k] = s[k];
+					if (map[i][j][k] == 'S') {
+						sz = i;
+						sx = j;
+						sy = k;
+					}
+				}
+			}
+		}
+
+		queue<pair<int, pair<int, int>>> Q;
+		Q.push({ sz,{sx,sy} });
+
+		while (!Q.empty()) {
+			int size = Q.size();
+
+			while (size--) {
+				int z = Q.front().first;
+				int x = Q.front().second.first;
+				int y = Q.front().second.second;
+				Q.pop();
+
+				if (map[z][x][y] == 'E') {
+					Flag = true;
+					break;
+				}
+
+				for (int k = 0; k < 6; k++) {
+					int nz = z + dz[k];
+					int nx = x + dx[k];
+					int ny = y + dy[k];
+
+					if (nz >= 0 && nz < L && nx >= 0 && nx < R && ny >= 0 && ny < C && !visit[nz][nx][ny]) {
+						if ((map[nz][nx][ny] == '.' || map[nz][nx][ny] == 'E')) {
+							visit[nz][nx][ny] = true;
+							Q.push({ nz,{nx,ny} });
+						}
+					}
+				}
+			}
+			if (Flag) break;
+			cnt++;
+		}
+		if(Flag) cout << "Escaped in " << cnt << " minute(s)." << '\n';
+		else cout << "Trapped!" << '\n';
+	}
+
+	return 0;
+}
+
---
+++ 서울 지하철 2호선.cpp
@@ -0,0 +1,97 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+using namespace std;
+int visit[3001];
+int cycle[3001];
+int map[3001][3001];
+int n, x, y;
+int check;
+
+void DFS(int x, int pre) {
+	
+	for (int y = 1; y <= n; y++) {
+		if (map[x][y] && y != pre && visit[y]!=2) {
+			visit[y]++;
+			if (visit[y]==2) {
+				check = y;
+				return;
+			}
+			DFS(y, x);
+		}
+	}
+}
+
+void DFS_2(int x, int pre, int start) {
+	if (visit[start]) {
+		for (int i = 1; i <= n; i++) {
+			cycle[i] = visit[i];
+		}
+		return;
+	}
+
+	for (int y = 1; y <= n; y++) {
+		if (!visit[y] && map[x][y] && pre != y) {
+			visit[y] = true;
+			DFS_2(y, x, start);
+			visit[y] = false;
+		}
+	}
+}
+
+void BFS() {
+	queue<int> Q;
+	for (int i = 1; i <= n; i++) {
+		if (cycle[i]) {
+			Q.push(i);
+			visit[i] = 0;
+			break;
+		}
+	}
+
+	while (!Q.empty()) {
+		int x = Q.front();
+		Q.pop();
+
+		for (int y = 1; y <= n; y++) {
+			if (visit[y] == -1 && map[x][y]) {
+				if (cycle[y]) {
+					visit[y] = 0;
+					Q.push(y);
+				}
+				else {
+					visit[y] = visit[x] + 1;
+					Q.push(y);
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> x >> y;
+		map[x][y] = map[y][x] = 1;
+	}
+
+	visit[1] = 1;
+	DFS(1, 1);
+	
+	memset(visit, 0, sizeof(visit));
+	DFS_2(check, check, check);
+
+	memset(visit, -1, sizeof(visit));
+	BFS();
+
+	for (int i = 1; i <= n; i++) cout << visit[i] << ' ';
+	cout << '\n';
+	return 0;
+}
+
