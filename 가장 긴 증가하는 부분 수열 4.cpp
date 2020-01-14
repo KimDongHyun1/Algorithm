349d0aed8e279473718fc033daded474f281f877
donghyun <donghyun4012@naver.com>
2020-01-14 16:04:07 +0900

    DP

---
+++ 가장 긴 증가하는 부분 수열 4.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int a[1001];
+int dp[1001];
+int n;
+vector<int> v[1001];
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	for (int i = 0; i < n; i++) {
+		v[i].push_back(a[i]);
+		for (int j = 0; j < i; j++) {
+			if (a[j] < a[i] && v[j].size()+1 > v[i].size()) {
+				for (int k = 0; k < v[j].size(); k++) {
+					v[i][k] = v[j][k];
+				}
+				v[i].push_back(a[i]);
+			}
+		}
+	}
+
+	int Max = 0;
+	int index = 0;
+	for (int i = 0; i < n; i++) {
+		if (Max < v[i].size()) {
+			Max = v[i].size();
+			index = i;
+		}
+	}
+
+	cout << v[index].size() << '\n';
+	for (int i = 0; i < v[index].size(); i++) {
+		cout << v[index][i] << ' ';
+	}
+
+	return 0;
+}
+
