7af680987a1d77d11c861ac20edff447c51cff4e
donghyun <donghyun4012@naver.com>
2020-01-29 22:10:13 +0900

    시뮬레이션

---
+++ 주사위 굴리기.cpp
@@ -0,0 +1,106 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int dice[7] = { 0,0,0,0,0,0,0 };
+int map[21][21];
+int dx[] = { 0, 0,0,-1,1 }; // 동 서 북 남
+int dy[] = { 0, 1,-1,0,0 };
+int n, m, x, y, k, dir, temp1, temp2;
+
+void output() {
+	if (map[x][y]) {
+		dice[6] = map[x][y];
+		map[x][y] = 0;
+	}
+	else {
+		map[x][y] = dice[6];
+	}
+	cout << dice[1] << '\n';
+
+	/*for (int i = 1; i <= 6; i++) cout << dice[i] << ' ';
+	cout << '\n';
+*/
+	return;
+}
+
+int main(void) {
+	cin >> n >> m >> x >> y >> k;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	
+	while (k--) {
+		cin >> dir;
+		if (x + dx[dir] < 0 || x + dx[dir] >= n || y + dy[dir] < 0 || y + dy[dir] >= m) continue;
+
+		x += dx[dir];
+		y += dy[dir];
+		
+		if (dir == 1) {
+			temp1 = dice[4];
+			temp2 = dice[1];
+
+			dice[4] = dice[6];
+			dice[1] = temp1;
+
+			temp1 = dice[3];
+
+			dice[3] = temp2;
+			dice[6] = temp1;
+
+			output();
+		}
+		else if (dir == 2) {
+			temp1 = dice[3];
+			temp2 = dice[1];
+
+			dice[3] = dice[6];
+			dice[1] = temp1;
+
+			temp1 = dice[4];
+
+			dice[4] = temp2;
+			dice[6] = temp1;
+
+			output();
+		}
+		else if (dir == 3) {
+			temp1 = dice[5];
+			temp2 = dice[1];
+
+			dice[5] = dice[6];
+			dice[1] = temp1;
+
+			temp1 = dice[2];
+
+			dice[2] = temp2;
+			dice[6] = temp1;
+
+			output();
+		}
+		else if (dir == 4) {
+			temp1 = dice[2];
+			temp2 = dice[1];
+
+			dice[2] = dice[6];
+			dice[1] = temp1;
+			
+			temp1 = dice[5];
+			
+			dice[5] = temp2;
+			dice[6] = temp1;
+
+			/*
+			dice[2] = dice[6];
+			dice[1] = dice[2];
+			dice[5] = dice[1];
+			dice[6] = dice[5];*/
+
+			output();
+		}
+	}
+}
---
+++ 톱니바퀴.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <deque>
+using namespace std;
+int t, x, watch, ans;
+deque<int> D[10];
+string s;
+
+//시계
+void go(int n) {
+	D[n].push_front(D[n].back());
+	D[n].pop_back();
+}
+
+void back(int n) {
+	D[n].push_back(D[n].front());
+	D[n].pop_front();
+}
+
+int main()
+{
+	for (int i = 1; i <= 4; i++) {
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) {
+			D[i].push_back(s[j] - '0');
+		}
+	}
+
+	cin >> t;
+	while (t--) {
+		cin >> x >> watch;
+
+		if (x == 1) {
+			if (D[1][2] != D[2][6]) {
+				if (D[2][2] != D[3][6]) {
+					if (D[3][2] != D[4][6]) {
+						if (watch == 1) back(4);
+						else go(4);
+					}
+					if (watch == 1) go(3);
+					else back(3);
+				}
+				if (watch == 1) back(2);
+				else go(2);
+			}
+			if (watch == 1) go(1);
+			else back(1);
+		}
+
+		if (x == 2) {
+			if (D[1][2] != D[2][6]) {
+				if (watch == 1) back(1);
+				else go(1);
+			}
+			if (D[2][2] != D[3][6]) {
+				if (D[3][2] != D[4][6]) {
+					if (watch == 1) go(4);
+					else back(4);
+				}
+				if (watch == 1) back(3);
+				else go(3);
+			}
+			if (watch == 1) go(2);
+			else back(2);
+		}
+
+		if (x == 3) {
+			if (D[2][2] != D[3][6]) {
+				if (D[1][2] != D[2][6]) {
+					if (watch == 1) go(1);
+					else back(1);
+				}
+				if (watch == 1) back(2);
+				else go(2);
+			}
+			if (D[3][2] != D[4][6]) {
+				if (watch == 1) back(4);
+				else go(4);
+			}
+			if (watch == 1) go(3);
+			else back(3);
+		}
+
+		if (x == 4) {
+			if (D[3][2] != D[4][6]) {
+				if (D[2][2] != D[3][6]) {
+					if (D[1][2] != D[2][6]) {
+						if (watch == 1) back(1);
+						else go(1);
+					}
+					if (watch == 1) go(2);
+					else back(2);
+				}
+				if (watch == 1) back(3);
+				else go(3);
+			}
+			if (watch == 1) go(4);
+			else back(4);
+		}
+
+	}
+
+	if (D[1][0]) ans += 1;
+	if (D[2][0]) ans += 2;
+	if (D[3][0]) ans += 4;
+	if (D[4][0]) ans += 8;
+
+	cout << ans << '\n';
+}
