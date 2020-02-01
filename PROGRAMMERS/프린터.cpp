cc81abfb38d66cb3ffbe86ceb1ecbde538486b94
donghyun <donghyun4012@naver.com>
2020-02-02 02:19:57 +0900

    스택/큐

---
+++ PROGRAMMERS/프린터.cpp
@@ -0,0 +1,39 @@
+﻿#include <vector>
+#include <queue>
+using namespace std;
+priority_queue<int> PQ;
+queue<pair<int, int>> Q;
+
+int solution(vector<int> priorities, int location) {
+	int ans = 1;
+
+	for (int i = 0; i < priorities.size(); i++) {
+		Q.push({ priorities[i], i });
+		PQ.push(priorities[i]);
+	}
+
+	bool flag = false;
+	while (true) {
+		while (Q.front().first != PQ.top()) {
+			Q.push({ Q.front().first, Q.front().second });
+			Q.pop();
+		}
+
+		while (Q.front().first == PQ.top()) {
+			if (Q.front().second == location) {
+				flag = true;
+				break;
+			}
+			else {
+				PQ.pop();
+				Q.pop();
+				ans++;
+			}
+		}
+
+		if (flag) break;
+	}
+
+
+	return ans;
+}
