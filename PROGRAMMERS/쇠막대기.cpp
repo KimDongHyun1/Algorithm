11d2ebf9e387c16d6d7a19a57fa335ded1f6a864
donghyun <donghyun4012@naver.com>
2020-02-02 02:27:30 +0900

    스택/큐

---
+++ PROGRAMMERS/쇠막대기.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string arrangement) {
+	int ans = 0;
+	int cnt = 0;
+
+	for (int i = 0; i < arrangement.size(); i++) {
+		if (arrangement[i] == '(') cnt++;
+		else {
+			if (arrangement[i - 1] == ')') {
+				cnt--;
+				ans += 1;
+			}
+			else {
+				cnt--;
+				ans += cnt;
+			}
+		}
+	}
+
+
+	return ans;
+}
+
+
