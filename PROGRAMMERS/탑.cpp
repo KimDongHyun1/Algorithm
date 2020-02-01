9a3b7ba2a12fecc12c8f7ad4943d5ae7f1286c32
donghyun <donghyun4012@naver.com>
2020-02-01 16:17:46 +0900

    스택/큐

---
+++ PROGRAMMERS/탑.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+stack<pair<int, int>> S;
+vector<int> heights;
+
+
+vector<int> solution(vector<int> heights) {
+	vector<int> answer;
+
+	for (int i = 0; i < heights.size(); i++) {
+
+		while (!S.empty() && S.top().first <= heights[i]) {
+			S.pop();
+		}
+
+		if (S.empty()) {
+			answer.push_back(0);
+		}
+		else {
+			answer.push_back(S.top.second);
+		}
+
+		S.push({ heights[i], i + 1 });
+
+	}
+
+	return answer;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		heights.push_back(x);
+	}
+
+	solution(heights);
+}
