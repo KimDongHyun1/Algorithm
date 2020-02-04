98320535b37f724318616d01cf1a9d329d7c99a4
donghyun <donghyun4012@naver.com>
2020-02-04 14:01:45 +0900

    스택/큐

---
+++ PROGRAMMERS/주식가격.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+stack<pair<int, int>> S;
+
+vector<int> solution(vector<int> prices) {
+	vector<int> ans(prices.size(), 0);
+	// prices(1,2,3,2,3) -> [4,3,1,1,0]
+	for (int i = 0; i < prices.size(); i++) {
+		if (S.empty()) S.push({ prices[i], i });
+		else {
+			while (!S.empty() && S.top().first > prices[i]) {
+				ans[S.top().second] = i - S.top().second;
+				S.pop();
+			}
+			S.push({ prices[i], i });
+		}
+	}
+
+
+	while (!S.empty()) {
+		ans[S.top().second] = prices.size() - 1 - S.top().second;
+		S.pop();
+	}
+
+	return ans;
+}
+
+
+
